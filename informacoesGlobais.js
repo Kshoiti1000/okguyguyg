7d5ed7483a5e73ce5ebc5a59c18d4462d5d9f8e9
Kshoiti1000 <kevinshoiti05@gmail.com>
2024-11-05 11:06:06 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -3,7 +3,11 @@ const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 async function visualizarinformacoesGlobais() {
     const res = await fetch (url)
     const dados = await res.json () 
-    console.log(dados); 
+    const paragrafo = document.createElement(`p`)
+    paragrafo.classList.add(`graficos-container__texto`)
+    paragrafo.innerHTML = `Sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e 
+    que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e 
+    passam em média ${dados.tempo_medio} horas `
 }
 
 visualizarinformacoesGlobais()
